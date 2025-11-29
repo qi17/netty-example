fe7ec892b82f3b4a2bee086675f90f230cbd5a46
qich <qch1996@aliyun.com>
2025-11-29 20:49:06 +0800

    netty-springboot

--- netty-demo-01/src/main/java/org/itstack/demo/netty/server/MyServerHandler.java
+++ netty-demo-01/src/main/java/org/itstack/demo/netty/server/MyServerHandler.java
@@ -9,11 +9,7 @@ import org.slf4j.LoggerFactory;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-/**
- * 虫洞栈：https://bugstack.cn
- * 公众号：bugstack虫洞栈  ｛获取学习源码｝
- * Create by fuzhengwei on 2019
- */
+
 public class MyServerHandler extends ChannelInboundHandlerAdapter {
 
     private Logger logger = LoggerFactory.getLogger(MyServerHandler.class);
@@ -24,11 +20,6 @@ public class MyServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         SocketChannel channel = (SocketChannel) ctx.channel();
-        logger.info("链接报告开始");
-        logger.info("链接报告信息：有一客户端链接到本服务端");
-        logger.info("链接报告IP:{}", channel.localAddress().getHostString());
-        logger.info("链接报告Port:{}", channel.localAddress().getPort());
-        logger.info("链接报告完毕");
         //通知客户端链接建立成功
         String str = "通知客户端链接建立成功" + " " + new Date() + " " + channel.localAddress().getHostString() + "\r\n";
         ctx.writeAndFlush(str);
@@ -44,9 +35,7 @@ public class MyServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        //接收msg消息{与上一章节相比，此处已经不需要自己进行解码}
         logger.info(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " 服务端接收到消息：" + msg);
-        //通知客户端链消息发送成功
         String str = "服务端收到：" + new Date() + " " + msg + "\r\n";
         ctx.writeAndFlush(str);
     }
